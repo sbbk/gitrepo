5897b51e7dfd500124ef1d4c1be6f516a8d45a47
sudosumo <turner.ell@gmail.com>
2013-04-16 16:13:24 +0900

    conky todo

--- todo.sh
+++ todo.sh
@@ -5,7 +5,7 @@ read -p "Type here: " result;
 if [ "$result" == "exit" ]; then
 	exit	
 else
-	echo "$result" >> ~/.conkyrc
+	echo "$result" \$alignr $(date +%a) >> ~/.conkyrc
         echo "$result added to todo"
         sh ~/todo.sh
 
